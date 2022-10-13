b2961105c2cfc45dfb10d0d11c8509c8345bf870
xiaoxiaosasa888 <115699527+xiaoxiaosasa888@users.noreply.github.com>
2022-10-13 22:53:01 +0800

    pull commit

--- src/main/java/aa/bb/cc/test.java
+++ src/main/java/aa/bb/cc/test.java
@@ -9,5 +9,6 @@ public class test {
         System.out.println("hello master");
         System.out.println("hello hot-fix");
         System.out.println("push test");
+        System.out.println("pull commit");
     }
 }
